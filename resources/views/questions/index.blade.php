cd322f07f0ccc6b27b5d41e311d1c0c6112cf005
Parminder Singh <parminder@datiot.com>
2018-10-15 11:20:32 +0530

    add vote controls to question and answer

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -25,7 +25,7 @@
                         </div>
 
                         <div class="status {{ $question->status }}">
-                            <strong>{{ $question->answers_count }}</strong> {{ str_plural('answers_count', $question->answers_count)}}
+                            <strong>{{ $question->answers_count }}</strong> {{ str_plural('answer', $question->answers_count)}}
                         </div>
 
                         <div class="view">
