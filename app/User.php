381d111151e2755efbc68ee273275ab59cb41254
Parminder Singh <parminder@datiot.com>
2018-10-18 11:50:41 +0530

    make the vote answer working

--- app/User.php
+++ app/User.php
@@ -79,5 +79,24 @@ class User extends Authenticatable
         $question->votes_count = $upVotes + $downVotes;
         $question->save();
     }
+
+    public function voteAnswer(Answer $answer, $vote) {
+        $voteAnswers = $this->voteAnswers();
+
+        if($voteAnswers->where('votable_id', $answer->id)->exists()) {
+            $voteAnswers->updateExistingPivot($answer, ['vote' => $vote]);
+        } else {
+            $voteAnswers->attach($answer, ['vote' => $vote]);
+        }
+
+        // //events doesn't work in many to many
+
+        $answer->load('votes'); //to refresh $answer 
+        $downVotes = (int) $answer->downVotes()->sum('vote');
+        $upVotes = (int) $answer->upVotes()->sum('vote');
+        
+        $answer->votes_count = $upVotes + $downVotes;
+        $answer->save();
+    }
     
 }
